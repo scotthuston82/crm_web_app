d3fc8c616707cf482a35f1e0457a0eb646f9e190
Scott <scotthuston1982@gmail.com>
2018-01-08 14:53:40 -0500

    final commit

--- crm.rb
+++ crm.rb
@@ -9,6 +9,10 @@ get '/index' do
   erb :index
 end
 
+get '/contacts/new' do
+  erb :new
+end
+
 get '/contacts' do
   @contacts = Contact.all
   erb :contacts
@@ -18,6 +22,51 @@ get '/contacts/:id' do
   @contact = Contact.find_by({id: params[:id].to_i})
   if @contact
     erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+post '/contacts' do
+  Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
+  redirect to('/contacts')
+end
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/contacts')
   else
     raise Sinatra::NotFound
   end
